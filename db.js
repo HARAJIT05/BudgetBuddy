7b97f26f89e8b068430b7589dc8898740d8f3d8a
HARAJIT <110291590+HARAJIT05@users.noreply.github.com>
2025-03-17 18:29:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017', {
+        await mongoose.connect('mongodb+srv://kazuha31:kazuha31@cluster0.ht9ceff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useUnifiedTopology: true
         });
         console.log('Connected to MongoDB');
